0bdf6e0477678a5318474cf0e5758566a121ec4d
Jon Wedell <jonwedell@gmail.com>
2017-02-16 08:00:56 -0600

    Add script to update date of wansview cameras.

---
+++ update_wansview_date.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+hosts=('192.168.1.210' '192.168.1.211')
+username=$1
+pass=$2
+now=`date +%Y-%m-%d\;%H:%M:%S`
+
+for host in ${hosts[*]}; do
+  echo "Updating time for: $host"
+  url="http://${host}/hy-cgi/device.cgi?cmd=setsystime&stime=${now}&timezone=9"
+#  echo "curl --digest -u $username:$pass -X GET $url"
+  curl --digest -u $username:$pass -X GET $url
+done
