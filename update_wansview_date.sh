115eccbeea958921299a9a5e03f7c6e99d2453b3
Jon Wedell <jonwedell@gmail.com>
2017-03-18 09:43:55 -0500

    Tweak camera script.

--- update_wansview_date.sh
+++ update_wansview_date.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-hosts=('192.168.1.210' '192.168.1.211')
+hosts=('192.168.1.210' '192.168.1.211' '192.168.1.212')
 username=$1
 pass=$2
 now=`date +%Y-%m-%d\;%H:%M:%S`
 
 for host in ${hosts[*]}; do
   echo "Updating time for: $host"
-  url="http://${host}/hy-cgi/device.cgi?cmd=setsystime&stime=${now}&timezone=9"
+  url="http://${host}/hy-cgi/device.cgi?cmd=setsystime&stime=${now}&timezone=11"
 #  echo "curl --digest -u $username:$pass -X GET $url"
   curl --digest -u $username:$pass -X GET $url
 done
